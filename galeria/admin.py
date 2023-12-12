92f038ae9c31e8a34dc556fe8baf1b985c5f59af
Douglas Medina <medina.douglas.dmv@gmail.com>
2023-12-12 16:50:42 -0300

    admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "categoria","legenda", "foto")
+    list_display = ("id", "nome", "categoria","legenda", "publicacao")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
+    list_editable = ("publicacao",)
     list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
